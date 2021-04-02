888b20c002636c77464722f3ab04b8018562018f
KR3KZ <ikreekzi@gmail.com>
2021-04-02 22:32:23 +0200

    Initial commit

---
+++ PalindromeApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PalindromeApp
+{
+    public class Palindrome
+    {
+        private String chaineNormale;
+        private String chaineOriginale;
+        private String chaineInversee;
+        /*
+        public Palindrome(String Z) {
+            this.chaineOriginale = Z;
+        }
+        */
+        public Boolean verifierPalindrome(String chaine) {
+            //Lower the string
+            chaine = chaine.ToLower();
+
+            //Removing special char from the string
+            chaine = Regex.Replace(chaine, @"[^\w\.@-]", "", RegexOptions.None, TimeSpan.FromSeconds(1.5));
+
+            //Removing diacritics from the string
+            chaine = Program.convertirChaineSansAccent(chaine);
+
+            //Check if the string contains digits
+            bool isDigitPresent = chaine.Any(c => char.IsDigit(c));
+            if (isDigitPresent) return false;
+
+            //String to array
+            char[] charArray = chaine.ToCharArray();
+
+            //Reverse the array
+            Array.Reverse(charArray);
+
+            return new string(charArray) == chaine;
+        }
+    }
+    static class Program
+    {
+        // Exécution du Programme
+        static void Main(string[] args) {
+            String laChaine = "";
+            Console.WriteLine("Saisissez une phrase :");
+            laChaine = Console.ReadLine();
+            Palindrome monPal = new Palindrome();
+            if (monPal.verifierPalindrome(laChaine)) {
+                Console.WriteLine("c'est un palindrome");
+            } else {
+                Console.WriteLine("ce n'est pas un palindrome");
+            }
+            Console.ReadKey();
+        }
+
+        static public string convertirChaineSansAccent(string chaine) {
+            // Déclaration de variables
+            string accent = "ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûüÿÑñÇç";
+            string sansAccent = "AAAAAAaaaaaaOOOOOOooooooEEEEeeeeIIIIiiiiUUUUuuuuyNnCc";
+
+            // Conversion des chaines en tableaux de caractères
+            char[] tableauSansAccent = sansAccent.ToCharArray();
+            char[] tableauAccent = accent.ToCharArray();
+
+            // Pour chaque accent
+            for (int i = 0; i < accent.Length; i++) {
+                // Remplacement de l'accent par son équivalent sans accent dans la chaîne de caractères
+                chaine = chaine.Replace(tableauAccent[i].ToString(), tableauSansAccent[i].ToString());
+            }
+
+            // Retour du résultat
+            return chaine;
+        }
+    }
+}
---
+++ TestPalindrome/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPalindrome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPalindrome")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94f4b28e-4025-47b8-b88b-07043494812e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
